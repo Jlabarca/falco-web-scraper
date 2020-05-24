41881669badde0bbba2743bfbf237745c97c166c
Julio Labarca <jlabarca25@gmail.com>
2020-05-24 07:03:06 -0400

    Utils file

--- falco-core.js
+++ falco-core.js
@@ -1,35 +1,61 @@
-
 const falcoStatic = require("./falco-static-scraper");
+const utils = require("./falco-utils");
+const log = require("./setup/log-setup");
 
 module.exports = {
-    
-    async processSnapshots(snapshots) {
-        let allSnapshots = await snapshots.find()
+  async processSnapshots(snapshots) {
+    let allSnapshots = await snapshots.find();
 
-        allSnapshots.forEach(async snapshot => {
-            console.log("Processing "+snapshot.name);
+    allSnapshots.forEach(async (snapshot) => {
+      console.log("Processing " + snapshot.name);
 
-            let start = process.hrtime();
+      let start = process.hrtime();
 
-            if(snapshot.dynamic) {
-            console.log("dynamic not implemented yet");
-            } else {
-                let data = await falcoStatic.scrape(snapshot.url, snapshot.query);
-                snapshot.data = data;
-                snapshot.last_update = new Date();   
-                snapshots.update({_id: snapshot._id}, snapshot)
-            }
+      if (snapshot.dynamic) {
+        console.log("dynamic not implemented yet");
+      } else {
+        let data = await falcoStatic.scrape(snapshot.url, snapshot.query);
 
-            elapsed_time(start, snapshot.name);
+        data = processData(snapshot, data)
+        if(data != null){
+            commitData(snapshots, snapshot, data);
+        }
+      }
 
-        });
-        
-    }
-}
+      utils.elapsedTime(start, snapshot.name);
+    });
+  },
+};
 
-elapsed_time = function(start, note){
-    var precision = 3; // 3 decimal places
-    var elapsed = process.hrtime(start)[1] / 1000000; // divide by a million to get nano to milli
-    console.log(process.hrtime(start)[0] + " s, " + elapsed.toFixed(precision) + " ms - " + note); // print message + time
-    start = process.hrtime(); // reset the timer
+processData = function (snapshot, newData) {
+
+  //Keywords  
+  if (snapshot.keywords != null && snapshot.keywords.length > 0) {
+    newData = newData.filter((element) => {
+      element.keywords = [];
+
+      snapshot.keywords.forEach((keyword) => {
+        if (element.title.includes(keyword)) 
+            element.keywords.push(keyword);
+      });
+
+      return element.keywords.length != 0;
+    });
+  } 
+
+  //Simple comparison, ordered json serialization string
+  newData.sort((a, b) => (a.title > b.title ? 1 : b.title > a.title ? -1 : 0));
+  console.log(newData.length + " - "+ snapshot.data.length);
+  
+  if(JSON.stringify(newData) != JSON.stringify(snapshot.data))
+    return newData
+
+    return null;
+};
+
+commitData = function(snapshots, snapshot, newData) {
+  log.info("Commiting data change for "+snapshot.name)
+  snapshot.data = newData;
+  snapshot.last_update = new Date();
+  snapshots.update({ _id: snapshot._id }, snapshot);
 }
